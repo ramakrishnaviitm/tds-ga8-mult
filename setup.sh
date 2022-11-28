e838c012116932caeb86ae66441854e48c96de5b
ramakrishnaviitm <119276161+ramakrishnaviitm@users.noreply.github.com>
2022-11-28 21:59:56 +0530

    Add files via upload
    adding setup file

---
+++ setup.sh
@@ -0,0 +1,14 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"22ds2000117@student.onlinedegree.iitm.ac.in\"\n\
+
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
